6f7c1a9799478f7c0ae2012e9db62974be8b857f
Torres-09 <dla612@naver.com>
2022-12-12 15:20:05 +0900

    feat: data base mysql -> h2 , springboot 3.0.0 -> 2.7.3

--- src/main/java/com/OAuth2/config/SecurityConfig.java
+++ src/main/java/com/OAuth2/config/SecurityConfig.java
@@ -1,10 +1,24 @@
 package com.OAuth2.config;
 
-import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.web.SecurityFilterChain;
 
-@Configuration
-@EnableWebSecurity // spring security filter 가 스프링 필터 체인에 등록된다.
+@EnableWebSecurity // spring security filter 가 스프링 필터 체인에 등록된다. 여기에 Configuration 있기 때문에 추가하면 오류발생
 public class SecurityConfig{
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity httpSecurity) throws Exception {
+        httpSecurity
+                .csrf().disable()
 
+                .authorizeRequests()
+                .antMatchers("/user/**").authenticated()
+                .antMatchers("/manager/**").access("hasRole('ROLE_ADMIN') or hasRole('ROLE_MANAGER')")
+                .antMatchers("/admin/**").access("hasRole('ROLE_ADMIN')")
+
+                .anyRequest().permitAll();
+
+        return httpSecurity.build();
+    }
 }
