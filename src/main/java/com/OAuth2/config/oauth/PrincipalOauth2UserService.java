36164b941c8eb105e4dd16d1c49e7867c01edbdc
Torres-09 <dla612@naver.com>
2022-12-12 15:39:46 +0900

    feat: 네이버 로그인 기능 추가

--- src/main/java/com/OAuth2/config/oauth/PrincipalOauth2UserService.java
+++ src/main/java/com/OAuth2/config/oauth/PrincipalOauth2UserService.java
@@ -1,6 +1,9 @@
 package com.OAuth2.config.oauth;
 
 import com.OAuth2.config.auth.PrincipalDetails;
+import com.OAuth2.config.oauth.provider.GoogleUserInfo;
+import com.OAuth2.config.oauth.provider.NaverUserInfo;
+import com.OAuth2.config.oauth.provider.OAuth2UserInfo;
 import com.OAuth2.model.User;
 import com.OAuth2.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +14,9 @@ import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+import java.util.Optional;
+
 @Service
 public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
 
@@ -19,38 +25,53 @@ public class PrincipalOauth2UserService extends DefaultOAuth2UserService {
     @Autowired
     private UserRepository userRepository;
 
-
     // 구글로부터 받은 userRequest 데이터 대한 후처리 되는 함수
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
 
+        // code를 통해 구성한 정보
+        System.out.println("userRequest clientRegistration : " + userRequest.getClientRegistration());
+        // token을 통해 응답받은 회원정보
+        System.out.println("oAuth2User : " + oAuth2User);
+
         // 구글 로그인 -> code를 리턴(Oauth-Client라이브러리) -> AccessToken 요청
         // userRequset 정보 -> loadUser 함수 -> userProfile
-        String provider = userRequest.getClientRegistration().getClientId(); // google
-        String provideId = oAuth2User.getAttribute("sub");
-        String email = oAuth2User.getAttribute("email");
-        String username = provider + "_" + provideId;
-        String password = bCryptPasswordEncoder.encode("겟인데어");
-        String role = "ROLE_USER";
+        return processOAuth2User(userRequest, oAuth2User);
+    }
 
-        User userEntity = userRepository.findByUsername(username);
-
-        // 빌더 패턴을 이용하여 유저를 생성한 후에 DB에 저장한다.
-        if (userEntity == null) {
-            userEntity = User.builder()
-                    .username(username)
-                    .password(password)
-                    .email(email)
-                    .role(role)
-                    .provider(provider)
-                    .providerId(provideId)
-                    .build();
-            userRepository.save(userEntity);
-
+    private OAuth2User processOAuth2User(OAuth2UserRequest userRequest, OAuth2User oAuth2User) {
+        OAuth2UserInfo oAuth2UserInfo = null;
+        if (userRequest.getClientRegistration().getRegistrationId().equals("google")) {
+            System.out.println("구글 로그인 요청");
+            oAuth2UserInfo = new GoogleUserInfo(oAuth2User.getAttributes());
+        } else if (userRequest.getClientRegistration().getRegistrationId().equals("naver")) {
+            System.out.println("네이버 로그인 요청");
+            oAuth2UserInfo = new NaverUserInfo((Map)oAuth2User.getAttributes().get("response"));
+        } else {
+            throw new OAuth2AuthenticationException("이 외 소셜로그인은 지원하지 않습니다.");
         }
 
-        // Authentication 객체 생성 ( OAuth2User 타입 -> OAuth2 로그인 )
-        return new PrincipalDetails(userEntity, oAuth2User.getAttributes());
+        Optional<User> userOptional = userRepository.findByProviderAndProviderId(oAuth2UserInfo.getProvider(), oAuth2UserInfo.getProviderId());
+
+        User user;
+        if (userOptional.isPresent()) {
+            user = userOptional.get();
+            // user가 존재하면 update 해주기
+            user.setEmail(oAuth2UserInfo.getEmail());
+            userRepository.save(user);
+        } else {
+            // user의 패스워드가 null이기 때문에 OAuth 유저는 일반적인 로그인을 할 수 없음.
+            user = User.builder()
+                    .username(oAuth2UserInfo.getProvider() + "_" + oAuth2UserInfo.getProviderId())
+                    .email(oAuth2UserInfo.getEmail())
+                    .role("ROLE_USER")
+                    .provider(oAuth2UserInfo.getProvider())
+                    .providerId(oAuth2UserInfo.getProviderId())
+                    .build();
+            userRepository.save(user);
+        }
+
+        return new PrincipalDetails(user, oAuth2User.getAttributes());
     }
 }
--- src/main/java/com/OAuth2/repository/UserRepository.java
+++ src/main/java/com/OAuth2/repository/UserRepository.java
@@ -3,7 +3,11 @@ package com.OAuth2.repository;
 import com.OAuth2.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 // @Repository 어노테이션이 없어도 IoC가 된다. JpaRepository를 상속했기 때문에
 public interface UserRepository extends JpaRepository<User,Integer> {
     public User findByUsername(String username);
+
+    public Optional<User> findByProviderAndProviderId(String provider, String providerId);
 }
